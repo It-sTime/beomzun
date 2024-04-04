5f0ca5da4eb4e85c18ee1752bf50e98714cf53d0
beomzun <punpon53@naver.com>
2024-04-04 18:31:18 +0900

    1918 - 후위표기식

---
+++ week01_Stack_Queue/후위표기식_1918/Main.java
@@ -0,0 +1,66 @@
+package 후위표기식_1918;
+
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String s = br.readLine();
+        Stack<Character> stack = new Stack<>();
+        char tmp;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+                bw.write(c);
+            } else {
+                if (c == '(') {
+                    stack.push(c);
+                } else if (c == ')') {
+                    tmp = stack.pop();
+                    while (tmp != '(') {
+                        bw.write(tmp);
+                        tmp = stack.pop();
+                    }
+                } else {
+                    if (stack.isEmpty()) {
+                        stack.push(c);
+                    } else {
+                        while (priority(stack.peek()) >= priority(c)) {
+                            bw.write(stack.pop());
+                            if (stack.isEmpty()) {
+                                break;
+                            }
+                        }
+                        stack.push(c);
+                    }
+                }
+            }
+        }
+        while (!stack.isEmpty()) {
+            bw.write(stack.pop());
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    public int priority(char c) {
+        return switch (c) {
+            case '(' -> 0;
+            case '*', '/' -> 2;
+            default -> 1;
+        };
+    }
+}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Solution s = new Solution();
+        s.solution();
+    }
+}
+
+/**
+ * 제한시간 2초
+ * 이전에 풀었던 문제. 다시 봤을 때 기억이 잘 안나서 switch 로 고생함
+ */
